7355480c9e62db5a7b318cc068a7e7f5a37eaefc
Timóteo Stifft <timoteostifft@gmail.com>
2022-02-10 08:48:14 -0300

    feature: add api route versioning model

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,20 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix('v1')->group(function(){
+    Route::get('lista', function(){
+        return ['a','b','c'];
+    }); 
+
+    Route::post('cadastra', function(){
+        echo 'implementar';
+    });
+});
+
+Route::prefix('v2')->group(function(){
+    //
 });
