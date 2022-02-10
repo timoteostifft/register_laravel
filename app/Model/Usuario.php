26d89e1009397065349ccc1b91224d5cefe50009
Timóteo Stifft <timoteostifft@gmail.com>
2022-02-10 10:29:05 -0300

    feature: add user list model method

--- app/Model/Usuario.php
+++ app/Model/Usuario.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Usuario extends Model
 {
     protected $connection = 'sqlite';
+    protected $table = 'usuario';
+
+    public static function listar(int $limite){
+        $sql = self::select([
+            "id",
+            "nome",
+            "email",
+            "data_cadastro"
+        ])
+        ->limit($limite);
+
+        dd($sql->toSql());
+    }
 }
