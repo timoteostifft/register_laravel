7775f8f9ded1b2f660d9c998b01baaa1e04c9464
Timoteo stifft <timoteostifft@gmail.com>
2022-02-11 12:06:13 -0300

    feature: create form estructure

--- resources/views/usuario/cadastro.blade.php
+++ resources/views/usuario/cadastro.blade.php
@@ -3,5 +3,23 @@
 @section('title', 'Cadastro de usuário')
 
 @section('content')
-  <form action="{{route('salvar')}}"></form>
+  <form action="{{route('salvar')}}" method="post">
+    {{-- {{csrf_field()}} --}}
+
+    <div class="field">
+      <label for="nome">Nome:</label>
+      <input type="text" name="nome" id="nome">
+    </div>
+
+    <div class="field">
+      <label for="email">E-mail:</label>
+      <input type="text" name="email" id="email">
+    </div>
+
+    <div class="field">
+      <label for="nome">Senha:</label>
+      <input type="text" name="senha" id="senha">
+    </div>
+
+  </form>
 @endsection
